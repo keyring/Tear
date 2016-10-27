e5796eb83ea6ee2187dc790d017a345011a146af
keyring <keyrings@163.com>
2016-10-28 07:50:46 +0800

    windows support

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,41 @@
 -- premake5.lua
-workspace "TxxE"
-configurations { "Debug", "Release" }
-location "build"
+workspace "TearEngine"
+    configurations { "Debug", "Release" }
+    platforms {"x64", "x86"}
+    location "build"
 
-project "TxxE"
-kind "WindowedApp"
-language "C++"
-location "build/TxxE"
-libdirs {"libs", "/usr/local/lib"}
-links {"glfw3", "X11", "Xrandr", "Xi", "Xinerama", "Xxf86vm", "Xcursor", "GL", "pthread", "dl"}
-targetdir "bin/%{cfg.buildcfg}"
+    project "TearEngine"
+        kind "WindowedApp"
+        language "C++"
+        location "build/TearEngine"
 
-files { "**.h", "**.c", "**.cpp" }
+        targetdir "bin/%{cfg.buildcfg}"
 
-filter "configurations:Debug"
-defines { "DEBUG" }
-flags { "Symbols" }
+        files { "**.h", "**.c", "**.cpp" }
 
-filter "configurations:Release"
-defines { "NDEBUG" }
-optimize "On"
+        filter "configurations:Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+
+        filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+        configuration "windows"
+            links{
+                "glfw3",
+                "OpenGL32"
+            }
+        configuration "linux"
+            links{
+                "glfw3", 
+                "X11",
+                "Xrandr",
+                "Xi", 
+                "Xinerama",
+                "Xxf86vm", 
+                "Xcursor", 
+                "GL", 
+                "pthread", 
+                "dl"
+            }
